f74e320215456eff603d7816279428a3a02a508b
ThuHuyen731 <nguyenthuhuyen731@gmail.com>
2022-06-04 22:44:41 +0700

    Rendering a list of (dummy) meetups

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,34 @@
+import MeetupItem from "../components/meetups/MeetupItem";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meertup",
+    image:
+      "https://yt3.ggpht.com/yti/APfAmoGRXr6Ob6y40L_BDc84imxW9H5l6zbbDxRFUa1c0w=s88-c-k-c0x00ffffff-no-rj-mo",
+    address: "Some address 5, 1234 Some City",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "A Second Meertup",
+    image:
+      "https://yt3.ggpht.com/yti/APfAmoGRXr6Ob6y40L_BDc84imxW9H5l6zbbDxRFUa1c0w=s88-c-k-c0x00ffffff-no-rj-mo",
+    address: "Some address 110, 1234 Some City",
+    description: "This is a second meetup",
+  },
+  {
+    id: "m3",
+    title: "A Third Meertup",
+    image:
+      "https://yt3.ggpht.com/yti/APfAmoGRXr6Ob6y40L_BDc84imxW9H5l6zbbDxRFUa1c0w=s88-c-k-c0x00ffffff-no-rj-mo",
+    address: "Some address 54, 1234 Some City",
+    description: "This is a third meetup",
+  },
+];
+
 function HomePage() {
-  return <h2>HomePage</h2>;
+  return <MeetupItem meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
