3ba3d8a76caea7fa297986fe7a2b6bf627294f99
ThuHuyen731 <nguyenthuhuyen731@gmail.com>
2022-06-04 21:58:36 +0700

    Linking Between Pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,22 @@
+import { Fragment } from "react";
+import Link from "next/link";
+
 function NewPage() {
-  return <h2>News Page</h2>;
+  return (
+    <Fragment>
+      <h2>News Page</h2>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextIf is a great framework
+          </Link>
+        </li>
+        <li>
+          <a href="/news/something-else">Something Else</a>
+        </li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewPage;
