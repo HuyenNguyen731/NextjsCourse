95ab5b59b037f712b04eec06862e511621302da5
ThuHuyen731 <nguyenthuhuyen731@gmail.com>
2022-06-20 15:39:13 +0700

    Exploring getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -31,14 +31,27 @@ function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
+  const req = content.req;
+  const res = content.res;
+
   // fetch data from an API
+
   return {
     props: {
       meetups: DUMMY_MEETUPS,
     },
-    revalidate: 1,
   };
 }
+
+// export async function getStaticProps() {
+//   // fetch data from an API
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//     revalidate: 1,
+//   };
+// }
 
 export default HomePage;
